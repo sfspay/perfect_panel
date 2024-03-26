3989fb9b3c5c9dcba1f207bdaba841b4af26cf28
Abu Sayed Al Siam <abusayedalsiam2023@gmail.com>
2024-03-27 00:34:35 +0600

    update payment_methods.sql

--- perfect_panel_2/payment_methods.sql
+++ perfect_panel_2/payment_methods.sql
@@ -1,3 +1,3 @@
 INSERT INTO `payment_methods` (`id`, `method_name`, `method_get`, `method_min`, `method_max`, `method_type`, `method_extras`, `method_line`, `nouse`) VALUES
-(71, 'sfspay', 'sfspay', 1, 1000, '2', '{\"method_type\":\"2\",\"name\":\"SFS Pay\",\"min\":\"1\",\"max\":\"1000\",\"api_key\":\"491b7044d35923a9c90ea500f1eb2c8e07c3dd285a7b90ea\",\"api_url\":\"https:\\/\\/server.sfspanel.top\",\"exchange_rate\":\"110\"}', 1, '2'),
+(71, 'sfspay', 'sfspay', 1, 1000, '2', '{\"method_type\":\"2\",\"name\":\"SFS Pay\",\"min\":\"1\",\"max\":\"1000\",\"api_key\":\"491b7044d35923a9c90ea500f1eb2c8e07c3dd285a7b90ea\",\"api_url\":\"https:\\/\\/server.sfspanel.top\",\"exchange_rate\":\"110\"}', 1, '2');
 
